f4d815260d8c56553e9d056ade8197797b0dde49
unknown <A01707@HNT.DOMAIN>
2019-09-03 17:17:39 +0900

    added menu bar

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="todolistapp">
       <div id="header" class="header">
+          <div>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="#" @click="changeMenu('home')">Home</a>
+                  </li>
+                  <li>
+                      <a href="#" @click="changeMenu('about')">About</a>
+                  </li>
+                  <li>
+                      <a href="#" @click="changeMenu('contact')">Contact</a>
+                  </li>
+              </ul>
+          </nav>
+          </div>
+
+          <div>
+              <keep-alive include="about, home">
+              <component v-bind:is="currentView"></component>
+              </keep-alive>
+          </div>
+
           <h2>Todo List App</h2>
           <input-todo></input-todo>
       </div>
@@ -11,10 +33,22 @@
 <script type="text/javascript">
 import InputTodo from './InputTodo.vue';
 import List from './List.vue';
+import Home from './Home.vue';
+import About from './About.vue';
+import Contact from './Contact.vue';
 
 export default {
     name: 'todo-list',
-    components: {InputTodo, List }
+    components: {InputTodo, List, Home, About, Contact },
+    data() {
+        return { currentView:'home'}
+    },
+    methods: {
+        changeMenu(view) {
+            this.currentView = view;
+        }
+    }
+
 }
 </script>
 
